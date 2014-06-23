0df05086ab3b4539d033ecc619b236de76477daa
Florian Hussonnois <florian.hussonnois@zenika.com>
2014-06-23 23:41:04 +0200

    Fix NotSerializableExpections due to TupleMapper

--- src/main/java/com/github/fhuss/storm/elasticsearch/mapper/TupleMapper.java
+++ src/main/java/com/github/fhuss/storm/elasticsearch/mapper/TupleMapper.java
@@ -2,12 +2,14 @@ package com.github.fhuss.storm.elasticsearch.mapper;
 
 import backtype.storm.tuple.Tuple;
 
+import java.io.Serializable;
+
 /**
  * Interface for building document from {@link Tuple}.
  * @param <T>
 
  */
-public interface TupleMapper<T> {
+public interface TupleMapper<T> extends Serializable {
 
    T map(Tuple input);
 }
