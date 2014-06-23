6c8ffa547bf96d5715f2f7bc83f2ef9ff0853c06
Florian Hussonnois <florian.hussonnois@zenika.com>
2014-06-24 00:00:21 +0200

    Make TridentTupleMapper Serializable

--- src/main/java/com/github/fhuss/storm/elasticsearch/mapper/TridentTupleMapper.java
+++ src/main/java/com/github/fhuss/storm/elasticsearch/mapper/TridentTupleMapper.java
@@ -2,12 +2,14 @@ package com.github.fhuss.storm.elasticsearch.mapper;
 
 import storm.trident.tuple.TridentTuple;
 
+import java.io.Serializable;
+
 /**
  * Interface for building document from {@link storm.trident.tuple.TridentTuple}.
  * @param <T>
 
  */
-public interface TridentTupleMapper<T> {
+public interface TridentTupleMapper<T> extends Serializable {
 
    T map(TridentTuple input);
 }
